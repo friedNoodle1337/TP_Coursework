c0b5d4affea650bc14587fa0032905ccd65fa945
Artem Savranenko <savranenko.as@edu.spbstu.ru>
2022-09-01 20:00:46 +0300

    Delete comments

--- FT/HelpCommand.hpp
+++ FT/HelpCommand.hpp
@@ -11,27 +11,15 @@ namespace savranenko
     HelpCommand() = delete;
     HelpCommand(std::vector< std::string >& args, std::map< std::string, command_t >& mapOfCommands):
       args_(args),
-      description_("help - show list of available commands\n"),
-      mapOfCommands_(mapOfCommands)
-    {
-      //for (std::map< std::string, command_t >::const_iterator commandsIt = mapOfCommands.cbegin();
-      //  commandsIt != mapOfCommands.cend(); commandsIt++)
-      //{
-      //  descriptions_.push_back(commandsIt->second->getDescription());
-      //}
-    }
+      mapOfCommands_(mapOfCommands),
+      description_("help - show list of available commands\n")
+    {}
 
     void execute() override
     {
       checkExceptions();
 
       std::cout << "Available commands:\n";
-                //<< description_;
-      //for (std::size_t i = 0; i < descriptions_.size(); i++)
-      //{
-      //  std::cout << descriptions_[i];
-      //}
-
       for (std::map< std::string, command_t >::const_iterator commandsIt = mapOfCommands_.cbegin();
         commandsIt != mapOfCommands_.cend(); commandsIt++)
       {
@@ -46,9 +34,8 @@ namespace savranenko
 
   private:
     std::vector< std::string >& args_;
-    std::string description_;
     std::map< std::string, command_t >& mapOfCommands_;
-    //std::vector< std::string > descriptions_;
+    std::string description_;
 
     void checkExceptions() override
     {
